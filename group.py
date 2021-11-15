497af1c7d33754487284588a5ba3e1f38d700863
occ336 <e.gvp51.tid@telefonica.com>
2021-11-15 12:08:32 +0100

    modifie the group class, include self in get method:wq!

--- group.py
+++ group.py
@@ -2,7 +2,7 @@ from urllib.parse import urljoin
 
 class group:
   
-  def get(connection,baseurl):
+  def get(self,connection,baseurl):
     return conection.get(urljoin(baseurl, '/papi/v1/groups'))
   
    
