1c91de7d375c436e39f08717d82676a59977db9a
occ336 <e.gvp51.tid@telefonica.com>
2021-11-15 15:09:20 +0100

    fix some mistakes and now work properly

--- cpcode.py
+++ cpcode.py
@@ -1,78 +1,79 @@
 #!/usr/bin/env python3
 
-from urllib.parse import urljoin 
+from urllib.parse import urljoin
 
-class cpcodes:
-    
-    __init__(self,groupId,contractId):
-       
-       self.__groupId = groupId
-       self.__contractId = contractId
-       self.__papiurl="/papi/v1/cpcodes?{}&{}".format(groupId,contractId)
-       
-    def reader(JsonToread):
+class cpcode:
+
+    def  __init__(self,groupId,contractId):
+
+        self.__groupId = groupId
+        self.__contractId = contractId
+        self.__papiurl="/papi/v1/cpcodes?groupId={}&contractId={}".format(groupId,contractId)
+
+    def reader(self,response):
         """get all propieties in readly format"""
         for cpcode in response.json()['cpcodes']['items']:
              print("---------------------------\n \
                     cpcodeId {}\n \
                     cpcodeName : {}\n \
                     productIds : {}\n \
-                    createdDate : {}\n ".format((cpcode['cpcodeId'], \
+                    createdDate : {}\n ".format(cpcode['cpcodeId'], \
                                                  cpcode['cpcodeName'], \
                                                  cpcode['productIds'], \
-                                                 cpcode['createdDate'], \
-                                                )
-                                                
-                          )
-    def getAll(sesson,baseurl,json=True):
+                                                 cpcode['createdDate'])
+                  )
+
+    def getAll(self,connection,baseurl,json=True):
         """get all propieties in json format"""
         headers = { 'PAPI-Use-Prefixes' : 'true' }
-        response=sesson.get(urljoin(baseurl, self.__papiurl),headers = headers)
-        
+        response=connection.get(urljoin(baseurl, self.__papiurl),headers = headers)
+
         if response.status_code != 200:
-            print ('Error {}'.format(response.status_code))
-          
+             print ('Error {}'.format(response.status_code))
+             print(response.json())
+
         else:
-            if json: 
+            if json:
                 """get all propieties in json format"""
                 return response.json()
-          
+
             else:
-                reader(response)
-       
-                
-    def getCPcode(sesson,baseurl,cpcodeId):
+                self.reader(response)
+
+
+    def getCPcode(self,connection,baseurl,cpcodeId,json=True):
         """get data for one propiety in json format"""
-        papiurl = '/papi/v1/cpcodes/{}?groupId={}&contractI={}'.format(cpcodeId,self.__groupId,self.__contractId)
+        papiurl = '/papi/v1/cpcodes/{}?groupId={}&contractId={}'.format(cpcodeId,self.__groupId,self.__contractId)
         headers = { 'PAPI-Use-Prefixes' : 'true' }
-         
-        response=sesson.get(urljoin(baseurl, papiurl),headers=headers)
-        
+
+        response=connection.get(urljoin(baseurl, papiurl),headers=headers)
+
         if response.status_code != 200:
             print ('Error {}'.format(response.status_code))
-          
+            print (response.json())
+
         else:
-            
-            return response.json()
-          
+            if json:
+                return response.json()
+
             else:
-                reader(response)
-                                                
-                  
-    def createCPcode(sesson,baseurl,productId,cpcodeName,Json=True):
-        
+                self.reader(response)
+
+
+    def createCPcode(self,connection,baseurl,productId,cpcodeName,json=True):
         """create a new CPcode"""
-                send_data = """
-                      { 
+        send_data = """
+                      {
                           "productId": "%s",
                           "cpcodeName": "%s"
                       }""" % (productId,cpcodeName)
-                      
+
         headers = { 'Content-Type' : 'application/json' , 'PAPI-Use-Prefixes' : 'true' }
-        result = sesson.post(urljoin(baseurl, self.__papiurl),data=sent_data,headers=headers)
-        
+        response = connection.post(urljoin(baseurl, self.__papiurl),data=send_data,headers=headers)
+
         if response.status_code != 201:
             print ('Error {}'.format(response.status_code))
-          
+            print(response.json())
+ 
         else:
             return response.json()
